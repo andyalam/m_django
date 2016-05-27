4b30e2827643164116247c5f5ba465fa0dde0505
Andy  Alameddine <andya@bixly.com>
2016-05-27 13:42:05 -0700

    ch4

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Publisher, Author, Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
+admin.site.register(Publisher)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book)
--- books/models.py
+++ books/models.py
@@ -13,13 +13,13 @@ class Publisher(models.Model):
     website = models.URLField()
 
     def __str__(self):
-        return self.name
+        return u'%s %s' % (self.first_name, self.last_name)
 
 
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, verbose_name="e-mail")
 
     def __str__(self):
         return self.first_name + ' ' + self.first_name
@@ -29,7 +29,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
+    publication_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.title
