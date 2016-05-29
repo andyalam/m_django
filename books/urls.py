5ca64f6c5e6e1b3cef735a28318d174a7e40cf9c
andy <andyalam@hotmail.com>
2016-05-29 10:21:57 -0700

    end of ch6 core

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from books import views
 
 urlpatterns = [
-    url(r'^search-form/$', views.search_form),
     url(r'^search/$', views.search),
+    url(r'^meta/$', views.display_meta),
+    url(r'^contact/$', views.contact),
 ];
--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
 import datetime
 
 from books.models import Publisher, Author, Book
@@ -8,10 +10,43 @@ def search_form(request):
     return render(request, 'search_form.html')
 
 def search(request):
-    if 'q' in request.GET and request.GET['q']:
+    errors = []
+    if 'q' in request.GET:
         q = request.GET['q']
-        books = Book.objects.filter(title__icontains=q)
-        return render(request, 'search_results.html',
-            {'books': books, 'query': q})
+        if not q:
+            errors.append('Enter a search term.')
+        elif len(q) > 20:
+            errors.append('Please enter at most 20 characters.')
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render(request, 'search_results.html',
+                {'books': books, 'query': q})
+    return render(request, 'search_form.html', { 'errors': errors })
+
+
+def display_meta(request):
+    values = request.META.items()
+    values.sort()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['siteowner@example.com'],
+            )
+            return HttpResponseRedirect('/contact/thanks/')
     else:
-        return HttpResponse('Please submit a search term next time. DEAD ENDDD')
+        form = ContactForm(
+            initial={'subject': 'some initial vlauejkasdas'}
+        )
+    return render(request, 'contact_form.html', {'form': form})
